eff429e1f605c78090c8f651f6a221c76941305c
Koray Koska <koray@koska.at>
2018-04-10 00:28:37 +0200

    Restructure spm module

--- Package.swift
+++ Package.swift
@@ -6,13 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Clibgit2",
     pkgConfig: "libgit2",
-    products: [
-        .library(
-            name: "Clibgit2",
-            targets: ["Clibgit2"]
-        )
-    ],
-    targets: [
-        .target(name: "Clibgit2", dependencies: [])
+    providers: [
+        .brew(["libgit2"])
     ]
 )
